0586c7263048e346ecaf15447db7c6f5d5b5cdaa
Xy718 <zhefan.jin@nodeskai.com>
2026-03-23 18:18:48 +0800

    fix(selenium): Chrome 窗口大小读取环境变量，与 Xvfb 屏幕保持一致

--- services/selenium-chrome/start-browser.sh
+++ services/selenium-chrome/start-browser.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sleep 3
 export DISPLAY=:99.0
+W=${SE_SCREEN_WIDTH:-1280}
+H=${SE_SCREEN_HEIGHT:-800}
 exec /usr/lib/chromium/chromium \
   --no-sandbox \
   --test-type \
@@ -9,6 +11,6 @@ exec /usr/lib/chromium/chromium \
   --disable-blink-features=AutomationControlled \
   --disable-infobars \
   --lang=zh-CN \
-  --window-size=1920,1080 \
+  --window-size=${W},${H} \
   --start-maximized \
   https://www.baidu.com
